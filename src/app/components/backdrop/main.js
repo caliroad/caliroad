4fd8a205ab40676550d82765c180599b9a5aa2e5
pocco81 <58336662+pocco81@users.noreply.github.com>
2026-08-07 11:35:29 -0500

    fix(backdrop): properly use motion query and abstract pausing logic

--- src/app/components/backdrop/main.js
+++ src/app/components/backdrop/main.js
@@ -2,17 +2,18 @@ import "./styles.css"
 import backdropTemplate from "./template.html?raw"
 
 document.querySelector("main").insertAdjacentHTML("beforeend", backdropTemplate)
-
-// pause all CSS animations when the page is not visible
 const meshBackground = document.querySelector(".mesh-background")
 
-document.addEventListener("visibilitychange", () => {
-	if (document.hidden) {
+function assertPausedState(shouldPause) {
+	if (shouldPause) {
 		meshBackground.classList.add("paused")
 	} else {
 		meshBackground.classList.remove("paused")
 	}
-})
+}
+
+// pause all CSS animations when the page is not visible
+document.addEventListener("visibilitychange", () => assertPausedState(document.hidden))
 
 // pause animations when the page is not focused
 window.addEventListener("blur", () => {
@@ -26,8 +27,5 @@ window.addEventListener("focus", () => {
 })
 
 // pause animations if the user prefers reduced motion
-const prefersReducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)")
-
-if (prefersReducedMotion) {
-	meshBackground.classList.add("paused")
-}
+const motionQuery = window.matchMedia("(prefers-reduced-motion: reduce)")
+motionQuery.addEventListener("change", (e) => assertPausedState(e.matches))
