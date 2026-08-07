8ce5bc55d3f90c4eae5f0b70f1b700d932daafe5
pocco81 <58336662+pocco81@users.noreply.github.com>
2026-08-07 15:35:12 -0500

    feat(backdrop): add settings component

--- src/app/components/backdrop/main.js
+++ src/app/components/backdrop/main.js
@@ -1,31 +1,44 @@
 import "./styles.css"
 import backdropTemplate from "./template.html?raw"
+import { registerSetting } from "@app/components/settings-menu/main.js"
+import { backgroundAnimationToggleConfig } from "./components/settings/main.js"
 
 document.querySelector("main").insertAdjacentHTML("beforeend", backdropTemplate)
 const meshBackground = document.querySelector(".mesh-background")
 
-function assertPausedState(shouldPause) {
-	if (shouldPause) {
-		meshBackground.classList.add("paused")
-	} else {
-		meshBackground.classList.remove("paused")
-	}
+function pauseAnimations(shouldPause) {
+	if (shouldPause) meshBackground.classList.add("paused")
+	else meshBackground.classList.remove("paused")
 }
 
-// pause all CSS animations when the page is not visible
-document.addEventListener("visibilitychange", () => assertPausedState(document.hidden))
+function pauseAnimationsIfNoSetting(shouldPause) {
+	let savePreferences = localStorage.getItem("savePreferences") !== null
 
-// pause animations when the page is not focused
-window.addEventListener("blur", () => {
-	meshBackground.classList.add("paused")
-})
-
-window.addEventListener("focus", () => {
-	if (!document.hidden) {
-		meshBackground.classList.remove("paused")
-	}
-})
+	if (!savePreferences) pauseAnimations(shouldPause)
+}
 
 // pause animations if the user prefers reduced motion
 const motionQuery = window.matchMedia("(prefers-reduced-motion: reduce)")
-motionQuery.addEventListener("change", (e) => assertPausedState(e.matches))
+motionQuery.addEventListener("change", (e) => pauseAnimations(e.matches))
+
+// pause animations when the page is not visible
+document.addEventListener("visibilitychange", () => pauseAnimationsIfNoSetting(document.hidden))
+
+// pause animations when the page is not focused
+window.addEventListener("blur", () => pauseAnimationsIfNoSetting(true))
+window.addEventListener("focus", () => {
+	if (!document.hidden) pauseAnimationsIfNoSetting(false)
+})
+
+// pause animations when settings change
+window.addEventListener("background-animation-change", (e) => pauseAnimations(e.detail.isAnimationOn))
+
+let savePreferences = localStorage.getItem("savePreferences") !== null
+
+if (savePreferences) {
+	let savedBackgroundAnimationState = localStorage.getItem("savedBackgroundAnimationState")
+	console.log(savedBackgroundAnimationState)
+	if (savedBackgroundAnimationState !== null) pauseAnimations(!(savedBackgroundAnimationState === "true"))
+}
+
+registerSetting(backgroundAnimationToggleConfig)
